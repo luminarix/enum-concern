3a64a3b050b95b4cfc855a24c795759a463c2c0c
Mark <14284867+xHeaven@users.noreply.github.com>
2024-10-25 22:08:53 +0200

    Run the configuration

---
+++ src/EnumConcern.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Luminarix\EnumConcern;
+
+class EnumConcern {}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace Luminarix\Skeleton\Tests;
+namespace Luminarix\EnumConcern\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Luminarix\Skeleton\SkeletonServiceProvider;
+use Luminarix\EnumConcern\EnumConcernServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
@@ -13,14 +13,14 @@ class TestCase extends Orchestra
         parent::setUp();
 
         Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Luminarix\\Skeleton\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
+            fn (string $modelName) => 'Luminarix\\EnumConcern\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
         );
     }
 
     protected function getPackageProviders($app)
     {
         return [
-            SkeletonServiceProvider::class,
+            EnumConcernServiceProvider::class,
         ];
     }
 }
